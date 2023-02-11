815d4ceeee327b2f32267642277d5ee65225e1b2
baykan <baykan@reybex.com>
2023-02-11 16:03:13 +0300

    test

---
+++ client/ios/developeProje/main.m
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));Thread 1: "This method must not be called before the JS thread is created"
+  }
+}
