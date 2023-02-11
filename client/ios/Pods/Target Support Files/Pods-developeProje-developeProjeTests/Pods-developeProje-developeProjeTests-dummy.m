ce208fde1a53e97689f383f672a445125257c6f5
baykan <baykan@reybex.com>
2023-02-11 16:03:02 +0300

    test

---
+++ client/ios/Pods/Target Support Files/Pods-developeProje-developeProjeTests/Pods-developeProje-developeProjeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_developeProje_developeProjeTests : NSObject
+@end
+@implementation PodsDummy_Pods_developeProje_developeProjeTests
+@end
---
+++ client/ios/Pods/Target Support Files/Pods-developeProje/Pods-developeProje-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_developeProje : NSObject
+@end
+@implementation PodsDummy_Pods_developeProje
+@end
